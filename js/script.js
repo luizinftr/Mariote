4e1528b6cbb58ccf3b5f752e68e8864a732560a4
eduardo19791 <antenassatllite@gmail.com>
2024-03-09 23:08:48 -0300

    Atualizado

--- js/script.js
+++ js/script.js
@@ -1,6 +1,18 @@
 const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
+const gameBoard = document.querySelector('.game-board')
+const gameOver = document.querySelector('.game-over')
+const audioGameOver = new Audio('../assets/audio/gameover.mp3')
+let timeId 
 
+const restartGame = () => {
+   pipe.classList.add('pipeanimation');
+   pipe.style.removeProperty('left');
+   mario.src = './assets/images/mario3.gif';
+   mario.style.bottom = '0px';
+   gameOver.style.display = 'none';
+   GameStart()
+}
 const jump = () => {
 mario.classList.add('jump');
 
@@ -11,25 +23,32 @@ setTimeout(() => {
     }, 500);
 }
 
-const loop = setInterval(() => {
-
+const GameStart = () => {
+   console.log('ok'+timeId)
+   clearTimeout(timeId)
    const pipePosition = pipe.offsetLeft;
    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
 
    if(pipePosition <= 100 && pipePosition > 0 && marioPosition < 80) {
-
-     pipe.style.animation = ('none'); 
-     pipe.style.left = `${pipePosition}px`; 
+      audioGameOver.play()
+      pipe.classList.remove('pipeanimation');
+      pipe.style.left = `${pipePosition}px`; 
     
-     mario.style.animation = ('none'); 
+   //   mario.style.animation = 'none'; 
      mario.style.bottom = `${marioPosition}px`;
 
-     mario.src = './images/mario-game.over.png';
+     mario.src = './assets/images/mario-game.over.png';
      mario.style.width = '150px'
-
-     clearInterval(loop);
+     setTimeout(()=>{gameOver.style.display = 'flex'}, 5000)
+     return
    }
 
-}, 10);
+   timeId = setTimeout(()=>{GameStart()}, 10)
 
-document.addEventListener('keydown', jump);
+}
+document.addEventListener('keydown', ({keyCode})=>{
+   switch(keyCode){
+      case 32: jump(); break
+   }
+});
+GameStart()
