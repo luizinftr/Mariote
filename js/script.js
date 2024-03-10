e1084474a77cefc7712cf71f877e28cff2da5ac0
eduardo19791 <antenassatllite@gmail.com>
2024-03-09 23:27:13 -0300

    Atualizado

--- js/script.js
+++ js/script.js
@@ -3,14 +3,17 @@ const pipe = document.querySelector('.pipe');
 const gameBoard = document.querySelector('.game-board')
 const gameOver = document.querySelector('.game-over')
 const audioGameOver = new Audio('../assets/audio/gameover.mp3')
+const btn = document.querySelector('button')
 let timeId 
 
 const restartGame = () => {
+   gameBoard.style.filter = 'none'
    pipe.classList.add('pipeanimation');
    pipe.style.removeProperty('left');
    mario.src = './assets/images/mario3.gif';
    mario.style.bottom = '0px';
    gameOver.style.display = 'none';
+   btn.style.display = 'none'
    GameStart()
 }
 const jump = () => {
@@ -33,13 +36,14 @@ const GameStart = () => {
       audioGameOver.play()
       pipe.classList.remove('pipeanimation');
       pipe.style.left = `${pipePosition}px`; 
-    
+      gameBoard.style.filter = 'blur(5px)'
+      gameOver.style.display = 'flex'
    //   mario.style.animation = 'none'; 
      mario.style.bottom = `${marioPosition}px`;
 
      mario.src = './assets/images/mario-game.over.png';
      mario.style.width = '150px'
-     setTimeout(()=>{gameOver.style.display = 'flex'}, 5000)
+     setTimeout(()=>{btn.style.display = 'flex'}, 5000)
      return
    }
 
